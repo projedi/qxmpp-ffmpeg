71705558d87e7082a40961982da74795aefc632a
Alexander Shabalin <shabalyn.a@gmail.com>
2012-07-23 17:38:43 +0400

    Using yuv420p

--- src/base/QXmppCodec.cpp
+++ src/base/QXmppCodec.cpp
@@ -1341,7 +1341,6 @@ QXmppH264Encoder::QXmppH264Encoder() {
 }
 QXmppH264Encoder::~QXmppH264Encoder() { delete d; }
 
-//TODO: Properly recreate codec
 bool QXmppH264Encoder::setFormat(const QXmppVideoFormat &format)
 {
     d->codec = avcodec_find_encoder(CODEC_ID_H264);
@@ -1369,50 +1368,24 @@ QList<QByteArray> QXmppH264Encoder::handleFrame(const QXmppVideoFrame &qframe)
    pkt->size = 0;
    frame->width = qframe.width();
    frame->height = qframe.height();
-   if(qframe.pixelFormat() == QXmppVideoFrame::Format_YUV420P) {
-      frame->linesize[0] = qframe.bytesPerLine();
-      frame->linesize[1] = qframe.bytesPerLine()/2;
-      frame->linesize[2] = qframe.bytesPerLine()/2;
-      uchar* data = (uchar*) qframe.bits();
-      memcpy(frame->data[0], data, frame->linesize[0]*frame->height);
-      memcpy( frame->data[1], data + frame->linesize[0]*frame->height
-            , frame->linesize[1]*frame->height);
-      memcpy( frame->data[2], data + (frame->linesize[0]+frame->linesize[1])*frame->height
-            , frame->linesize[2]*frame->height);
-      for(int i = 3; i < AV_NUM_DATA_POINTERS; i++) {
-         frame->data[i] = 0;
-         frame->linesize[i] = 0;
-      }
-      frame->format = (int)PIX_FMT_YUV420P;
-   } else if(qframe.pixelFormat() == QXmppVideoFrame::Format_YUYV) {
-      frame->linesize[0] = qframe.bytesPerLine();
-      frame->data[0] = (uchar*)qframe.bits();
-      frame->format = (int)PIX_FMT_YUYV422;
-   } else if(qframe.pixelFormat() == QXmppVideoFrame::Format_RGB24) {
-      frame->linesize[0] = qframe.bytesPerLine();
-      frame->data[0] = (uchar*)qframe.bits();
-      frame->format = (int)PIX_FMT_RGB24;
-   }
-
-   AVFrame* newFrame = avcodec_alloc_frame();
-   d->scaler = sws_getCachedContext( d->scaler, frame->width, frame->height
-                                , (PixelFormat)frame->format
-                                , frame->width, frame->height, PIX_FMT_YUV420P
-                                , SWS_BICUBIC, 0, 0, 0);
-   avpicture_alloc( (AVPicture*)newFrame, PIX_FMT_YUV420P, frame->width, frame->height);
-   sws_scale( d->scaler, frame->data, frame->linesize, 0, frame->height
-            , newFrame->data, newFrame->linesize);
-
-   newFrame->pts = d->pts++;
+   frame->linesize[0] = qframe.bytesPerLine();
+   frame->linesize[1] = qframe.bytesPerLine()/2;
+   frame->linesize[2] = qframe.bytesPerLine()/2;
+   uchar* data = (uchar*) qframe.bits();
+   memcpy(frame->data[0], data, frame->linesize[0]*frame->height);
+   data+=frame->linesize[0]*frame->height;
+   memcpy(frame->data[1], data, frame->linesize[1]*frame->height)/2;
+   data+=frame->linesize[1]*frame->height/2;
+   memcpy(frame->data[2], data, frame->linesize[2]*frame->height/2);
+   frame->format = (int)PIX_FMT_YUV420P;
+   frame->pts = d->pts++;
    int got_packet = 0;
-   if(avcodec_encode_video2(d->codecContext,pkt,newFrame,&got_packet) < 0) return packets;
+   if(avcodec_encode_video2(d->codecContext,pkt,frame,&got_packet) < 0) return packets;
    if(!got_packet) { av_free_packet(pkt); return packets; }
    QByteArray packet((const char*)pkt->data,pkt->size);
    packets << packet;
    av_free_packet(pkt);
    av_free(frame);
-   avpicture_free((AVPicture*)newFrame);
-   av_free(newFrame);
    return packets;
 }
 
